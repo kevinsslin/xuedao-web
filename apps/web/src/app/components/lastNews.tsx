48bbcc643b8ae16612678f35e7c06318a0de5500
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2024-05-12 22:32:59 +0800

    fix:ig_photo_by_direct_embed

--- apps/web/src/app/components/lastNews.tsx
+++ apps/web/src/app/components/lastNews.tsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Box, Typography, Grid } from "@mui/material";
 
-import { Box, Typography } from "@mui/material";
-import WestIcon from "@mui/icons-material/West";
-import EastIcon from "@mui/icons-material/East";
-import ReplayIcon from "@mui/icons-material/Replay";
-import LockIcon from "@mui/icons-material/Lock";
-import Image from "next/image";
-import Link from "next/link";
+const instagramPosts = [
+  "https://www.instagram.com/p/C63ovPzykM4",
+  "https://www.instagram.com/p/C61NAC4roZ9",
+  "https://www.instagram.com/p/C6v6V9zNyym",
+];
 
 export function LastNews() {
+  useEffect(() => {
+    // Create the script tag for Instagram embed
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "//www.instagram.com/embed.js";
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Box className="flex w-full bg-gradient-to-b from-lastnews to-lastnews2 md:py-8 lg:py-12">
       <Box className="container flex flex-col mx-auto px-4 md:px-6">
         <Box className="items-center justify-center space-y-4 text-center">
           <Typography variant="h3">Last News</Typography>
         </Box>
-        <Box className="flex flex-col xl:flex-row justify-center items-center p-5 gap-5 px-auto">
-          <Image src="/news1.png" width={400} height={400} alt="news1" />
-          <Image src="/news2.png" width={400} height={400} alt="news2" />
-          <Image src="/news3.png" width={400} height={400} alt="news3" />
+        <Box className="gap-5 py-5 px-5">
+            <Grid container spacing={2}>
+                {instagramPosts.map((postUrl, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <blockquote
+                            className="instagram-media w-full"
+                            data-instgrm-permalink={postUrl}
+                            data-instgrm-version="14"
+                        />
+                    </Grid>
+                ))}
+            </Grid>
         </Box>
       </Box>
     </Box>
   );
 }
+
+export default LastNews;
